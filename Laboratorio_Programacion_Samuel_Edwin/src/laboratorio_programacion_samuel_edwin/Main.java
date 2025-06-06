0dddfb2dd6fada8289201758b4528c6a8dae79a8
Samuel Vasquez <83726280+Draccoo222@users.noreply.github.com>
2025-06-06 13:47:11 -0600

    Creacion del Main

---
+++ Laboratorio_Programacion_Samuel_Edwin/src/laboratorio_programacion_samuel_edwin/Main.java
@@ -0,0 +1,7 @@
+package laboratorio_programacion_samuel_edwin;
+public class Main {
+    public static void main(String[] args) {
+       MemoramaJuego juego = new MemoramaJuego();
+       juego.setVisible(true);
+    }
+}
