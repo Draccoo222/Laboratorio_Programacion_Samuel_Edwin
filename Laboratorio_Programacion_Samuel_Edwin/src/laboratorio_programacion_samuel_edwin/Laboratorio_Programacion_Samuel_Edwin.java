7862d3929d784db321a39e04f19cf8e8871c0525
Gabriel B) <weaselssh@gmail.com>
2025-06-06 13:22:08 -0600

    Imagenes

--- Laboratorio_Programacion_Samuel_Edwin/src/laboratorio_programacion_samuel_edwin/Laboratorio_Programacion_Samuel_Edwin.java
+++ Laboratorio_Programacion_Samuel_Edwin/src/laboratorio_programacion_samuel_edwin/Laboratorio_Programacion_Samuel_Edwin.java
@@ -1,20 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package laboratorio_programacion_samuel_edwin;
 
-/**
- *
- * @author unwir
- */
 public class Laboratorio_Programacion_Samuel_Edwin {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
         System.out.println("Hola mundo");
     }
     
