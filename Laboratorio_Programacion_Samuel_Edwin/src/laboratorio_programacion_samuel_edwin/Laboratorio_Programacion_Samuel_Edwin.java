1beb597a3cce5d34d8445e3b22a31ebeb8f06741
Samuel Vasquez <83726280+Draccoo222@users.noreply.github.com>
2025-06-06 13:13:31 -0600

    creacion

---
+++ Laboratorio_Programacion_Samuel_Edwin/src/laboratorio_programacion_samuel_edwin/Laboratorio_Programacion_Samuel_Edwin.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package laboratorio_programacion_samuel_edwin;
+
+/**
+ *
+ * @author unwir
+ */
+public class Laboratorio_Programacion_Samuel_Edwin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
