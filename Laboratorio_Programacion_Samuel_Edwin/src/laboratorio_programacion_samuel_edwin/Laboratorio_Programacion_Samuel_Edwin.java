12fbe6f52e31b81c9f03a861a1c0ee55461a8a35
Samuel Vasquez <83726280+Draccoo222@users.noreply.github.com>
2025-06-06 13:48:43 -0600

    Borrar archivo de prueba

--- Laboratorio_Programacion_Samuel_Edwin/src/laboratorio_programacion_samuel_edwin/Laboratorio_Programacion_Samuel_Edwin.java
+++
@@ -1,9 +0,0 @@
-package laboratorio_programacion_samuel_edwin;
-
-public class Laboratorio_Programacion_Samuel_Edwin {
-
-    public static void main(String[] args) {
-        System.out.println("Hola mundo");
-    }
-    
-}
